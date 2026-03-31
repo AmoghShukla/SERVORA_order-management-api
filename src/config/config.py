43b84642ce1f28e47a78cc1579265f8278b6474d
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-31 23:04:19 +0530

    implement database session management with SQLAlchemy

--- src/config/config.py
+++ src/config/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL : str
+    DATABASE_URL : str = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     class config:
         env_file = ".env"
 
--- src/database/session.py
+++ src/database/session.py
@@ -1,3 +1,16 @@
 from sqlalchemy import create_engine, sessionmaker
 from sqlalchemy.orm import Session
+import os
+from src.config.config import Settings
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
