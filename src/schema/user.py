c0f6c551bddeb38ed456d942e9538395f3cac75c
Amogh Shukla <amoghshukla548@gmail.com>
2026-04-05 14:59:03 +0530

    feat: update UserCreate and UserLogin models to include user_email and user_password fields

--- src/schema/user.py
+++ src/schema/user.py
@@ -2,5 +2,14 @@ from pydantic import BaseModel, Field, EmailStr
 
 class UserCreate(BaseModel):
     user_name: str = Field(..., min_length=2, max_length=50)
-    user_phone: str = Field(..., min_length=10, max_length=15)
     user_email: EmailStr
+    user_password : str = Field(..., min_length=10, max_length=15)
+    user_phone: str = Field(..., min_length=10, max_length=15)
+
+class UserLogin(BaseModel):
+    user_email : EmailStr
+    user_password : str = Field(..., min_length=10, max_length=15)
+
+class Token(BaseModel):
+    access_token : str
+    token_type : str = "bearer"
