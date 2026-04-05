29163c4156f8510ff54d926fc70de5ad61db428e
Amogh Shukla <amoghshukla548@gmail.com>
2026-04-05 20:06:09 +0530

    feat: refactor user service and repository structure, add error handling for user operations

--- src/repository/user.py
+++ src/repository/user.py
@@ -1,16 +1,28 @@
 from src.middleware.loggers import get_logger
 from sqlalchemy.orm import Session
 from src.model.user import User_Class
-from fastapi import HTTPException
+from sqlalchemy.exc import SQLAlchemyError
+from src.exceptions.custom_exception import RepositoryError, ServiceError, NotFoundError 
+from pydantic import EmailStr
 
 logger = get_logger(__name__)
+
+def get_user_by_email(email : EmailStr, db : Session):
+    try:
+        user = db.query(User_Class).filter(User_Class.user_email==email).first()
+        if not user:
+            raise NotFoundError(f"Task with Email ID : {email}, Does not exist")
+    except SQLAlchemyError as e:
+        raise RepositoryError("Failed to Fetch User with Email : {email}") from e
 
 def create_user(payload, db : Session):
     try:
         new_user = User_Class(
             user_name = payload.user_name,
             user_phone = payload.user_phone,
-            user_email = payload.user_email
+            user_email = payload.user_email,
+            user_password=payload.user_password,
+            user_role=payload.user_role if hasattr(payload, "user_role") else "USER"
         )
         logger.info(f"Creating user with payload: {payload}")
         db.add(new_user)
@@ -18,10 +30,9 @@ def create_user(payload, db : Session):
         db.refresh(new_user)
         logger.info(f"Order created successfully: {new_user}")
         return new_user
-    except Exception as e:
-        
+    except SQLAlchemyError as e:
         db.rollback()
-        raise HTTPException(status_code=500, detail=str(e))
+        raise RepositoryError("Failed to Create User") from e
 
 def get_user(user_id : int, db : Session):
     try:
--- src/routers/user.py
+++ src/routers/user.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from src.database.session import get_db
-from src.service import user_service
+from service import user
 from src.schema.user import UserCreate
 
 router = APIRouter(prefix="/User", tags=["User"])
 
 @router.post('/')
 def create_user(payload : UserCreate, db : Session = Depends(get_db)):
-    return user_service.create_user(payload, db)
+    return user.create_user(payload, db)
 
 @router.get('/{user_id}')
 def get_user(user_id : int, db : Session = Depends(get_db)):
-    return user_service.get_user(user_id, db)
+    return user.get_user(user_id, db)
